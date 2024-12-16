922b6e68e5007c7ee8389c4b630c12a0293f9dbd
mhemedabderrahmen <mhemedabderrahmen@gmail.com>
2024-12-16 22:55:04 +0000

    initialisation

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import { ThemeProvider } from "./components/theme-provider";
+
+export default function Document() {
+  return (
+      <Html lang="en">
+        <Head />
+        <body className="antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+  );
+}
